c14cba67088c5f7f9f657036f4d1c3025213d384
evan-krul <evan@krul.ca>
2022-04-04 22:18:47 -0600

    Added docker :)

--- server/index.js
+++ server/index.js
@@ -1,27 +1,24 @@
-// const path = require('path');
-// reference from https://github.com/ericterpstra/anagrammatix/blob/master/agxgame.js-
+// Setup basic express server
 const express = require('express');
-
-// define the Express function handler 
 const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
 
-// create a simple Express application
-app.configure(function() {
-    // Turn down the logging activity
-    app.use(express.logger('dev'));
-
-    // serve frontend from the public directory
-    // app.use(express.static(path.join(__dirname, 'public')));
+server.listen(port, () => {
+    console.log('Server listening at port %d', port);
 });
 
-// create a node.js based http server on port 8080
-const server = require('http').createServer(app).listen(process.env.PORT || 8080);
+
+app.get('/', (req, res) => {
+    res.send('Hello World! poop')
+})
 
 // create a Socket.IO server, and attaching it to the http server
-const io = require('socket.io').listen(server);
 
 // get controller with game session functions
-const gameController = require('./game_controller');
+// const gameController = require('./game_controller');
 
 // listen for Socket.IO connections
 io.sockets.on('connection', function(socket) {
--- view/src/App.js
+++ view/src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
 import RexAppBar from './components/NavBar';
 import Container from '@mui/material/Container';
+// import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4001";
+
 function App() {
+
+    // const [response, setResponse] = useState("");
+  
+    // useEffect(() => {
+    //   const socket = socketIOClient(ENDPOINT);
+    //   socket.on("FromAPI", data => {
+    //     setResponse(data);
+    //   });
+    // }, []);
+  
+
   return (
     <div className="App">
       <RexAppBar />
@@ -12,6 +26,8 @@ function App() {
           backgroundColor: '#ebebeb'
         }}
       >
+        Pepe
+        {/* {response} */}
       </Container>
     </div>
   );
